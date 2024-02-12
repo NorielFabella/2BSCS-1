4543efcf22a3407439272a9a071c3e1651838b85
Noriel Omar Fabella <152840848+NorielFabella@users.noreply.github.com>
2024-02-12 02:35:25 +0000

    This is my first commit file

---
+++ BlockSectionDA.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class BlockSectionDA
+{
+    private List<BlockSection> blockSectionList;
+
+    //Set Getter Method
+    public List<BlockSection> getBlockSectionList()
+    {
+        return blockSectionList;
+    }
+
+    //Create Constructor - Sets initial action of an object
+    public BlockSectionDA() throws FileNotFoundException
+    {
+        blockSectionList = new ArrayList<>();
+
+        Scanner blockFile = new Scanner (new FileReader("BlockFile.csv"));
+
+        while(blockFile.hasNext())
+        {
+            BlockSection blockSection = new BlockSection();
+
+            String blockSectionRaw = blockFile.nextLine();
+            String [] blockSectionArray = new String[2];
+            blockSectionArray = blockSectionRaw.split(", ");
+
+            //Set the values for the blockSection object
+            blockSection.setBlockCode(blockSectionArray[0].trim());
+            blockSection.setAdviser(blockSectionArray[1].trim());
+
+            StudentDA studentDA = new StudentDA(blockSection.getBlockCode());
+            blockSection.setStudentList(studentDA.getStudentList());
+
+            blockSectionList.add(blockSection);
+        }
+
+        blockFile.close();
+    }
+}
---
+++ Student.java
@@ -0,0 +1,42 @@
+public class Student
+{
+    private String studentNo;
+    private String lastName;
+    private String firstName;
+
+    //Setters
+
+    public void setStudentNo(String studentNo)
+    {
+        this.studentNo = studentNo;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    //Getters
+
+    public String getStudentNo()
+    {
+        return studentNo;
+    }
+    
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    
+
+}
